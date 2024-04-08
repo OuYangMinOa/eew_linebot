9abdab2e29eb7ba60332669087d233a97bd65674
OuYangMinOa <wesley91345@gmail.com>
2024-04-08 14:14:38 +0800

    test:for testing

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ async def START_SERVICES():
 
 if __name__ == "__main__":
     # # Start the ngrok service
-    # listener = ngrok.forward(PORT,authtoken_from_env=True)
-    # print(f"[*] Url : {listener.url()}")
+    listener = ngrok.forward(PORT,authtoken_from_env=True)
+    print(f"[*] Url : {listener.url()}")
 
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
