fc439c3749bd27d7e647d2866adf0f0b5a8d285f
OuYangMinOa <wesley91345@gmail.com>
2024-04-23 03:02:52 +0800

    fix: user id -> source id

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ from linebot.v3.messaging import Configuration
 from .user                import SubsribeController
 
 
-PORT = 9009
+PORT = 9010
 DATA_FOLDER = SubsribeController.DATA_FOLDER
 
 os.makedirs(DATA_FOLDER, exist_ok=True)
